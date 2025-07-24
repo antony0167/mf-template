35dd6c82a9d9059b2e6125f369006776deb90942
dluze <dluzewskiantoni@gmail.com>
2025-07-24 11:59:06 +0200

    Broadcast

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter([]),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,85 +1,14 @@
-import {Component, HostListener, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute, RouterOutlet} from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MfPost } from './mf-post/mf-post';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MfPost],
   templateUrl: './app.html',
   standalone: true,
   styleUrl: './app.css'
 })
-export class App implements OnInit, OnDestroy {
-  id = -1;
-  receivedId: number | undefined;
-  n = 10;
-  myInterval: any;
-
-  // Operations
-  count = 0;
-  sources: number[] = [];
-  operationValues: number[] = [];
-
-  constructor(private route: ActivatedRoute) {}
-
-
-  ngOnInit() {
-    this.route.queryParams.subscribe(params => {
-      if (params['id']) {
-        this.id = params['id'];
-      }
-    });
-
-    this.startCommunication();
-  }
-
-  startCommunication() {
-    this.myInterval = setInterval(() => {
-      this.sendMessage();
-    }, 5000);
-  }
-
-  sendMessage() {
-    let randomIds = () => {
-      let uniqueNumbers:Set<number> = new Set();
-      while (uniqueNumbers.size < 5) {
-        uniqueNumbers.add(Math.floor(Math.random() * this.n));
-      }
-      return Array.from(uniqueNumbers);
-    }
-
-    for (let targetId of randomIds()) {
-      window.parent.postMessage({
-        type: 'sendMessage',
-        detail: {
-          name: `data-mf-${this.id}`,
-          sourceId: this.id,
-          targetId: targetId,
-          ts: Date.now(),
-          random: Math.random()
-        }
-      }, 'http://localhost:4200/');
-    }
-  }
-
-  @HostListener('window:message', ['$event'])
-  handleMessage(event: MessageEvent) {
-
-    if (event.origin !== 'http://localhost:4200') return;
-
-
-    if (event.data?.type === 'sendMessage' && event.data.detail) {
-      const detail = event.data.detail;
-      this.receivedId = detail.sourceId;
-      this.count++;
-      this.sources.push(detail.sourceId);
-      this.operationValues.push(detail.sourceId + detail.targetId / detail.random)
-    }
-  }
-
-  ngOnDestroy() {
-    if (this.myInterval) {
-      clearInterval(this.myInterval);
-    }
-  }
+export class App {
 
 }
